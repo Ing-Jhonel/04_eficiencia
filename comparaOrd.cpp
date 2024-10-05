6d10ea7676473b85089ea4cb048b0007dc7ec230
Jhonel <jgapazap@unjbg.edu.pe>
2024-10-05 14:45:57 -0500

    Avance

--- comparaOrd.cpp
+++ comparaOrd.cpp
@@ -10,52 +10,80 @@ int busquedaBinaria(int a[], int, int);
 int main(){
 	
     srand(time(0));
-    int tamanio;
+    int tamanio, busquedas, valor;
+    char aleatorio;
     cout << "Indique la cantidad de elementos de su arreglo: ";
     cin >> tamanio;
+    cout << "Indique cuantas busquedas quiere hacer: ";
+    cin >> busquedas;
+    
+    bool seguir=true;
+    do{
+    	cout << "Buscar elemento aleatorio?(s/n): ";
+	    cin >> aleatorio;
+	    
+	    if(aleatorio=='s'){
+	    	valor=rand() % 100;
+		} else if(aleatorio=='n'){
+			cout << "Indique el elemento a buscar: ";
+			cin >> valor;
+		} else {
+			seguir=false;
+		}
+	} while(!seguir);
+	cout << endl;
     int arr[tamanio];
+    
     for(int i=0; i<tamanio; i++){
-        arr[i]= rand() % 100;
-        cout << arr[i] << " ";	
+        arr[i] = rand() % 100;
+        //cout << arr[i] << " ";	
     }
-    int valor;
-    cout << endl << "Indique el elemento a buscar: ";
-    cin >> valor;
+    //cout << endl << endl << "Arreglo ordenado:" << endl << endl;
+    ordenarBidireccional(arr, tamanio);
+    /*
+	for(int i=0; i<tamanio; i++){
+        cout << arr[i] << " ";	
+    } 
     cout << endl;
+	*/
+	
+	system("cls");
     int opcion;
-    cout << "Elija el tipo de busqueda: " << endl;
+	double tiempoBusquedaSecuOrd[busquedas], tiempobusquedaBinaria[busquedas];
+    cout << "Elija el tipo de busqueda: " << endl << endl;
     cout << "1) Busqueda secuencial en datos ordenados" << endl;
     cout << "2) Busqueda binaria " << endl << endl;
-    cin >> opcion;
+    cout << "= "; cin >> opcion;
 	
-    cout << endl;
-    ordenarBidireccional(arr, tamanio);
-    
-    clock_t inicio, fin;
-    double duracion;
-
+    system("cls");
     switch(opcion){
         case 1:
-            inicio = clock();
-            cout << "El valor " << valor << " esta en la posicion: " << busquedaSecuOrd(arr, tamanio, valor);
-            fin = clock();
-            duracion = double(fin - inicio) / CLOCKS_PER_SEC;
-            cout << endl << "Tiempo de ejecuciÃ³n (secuencial ordenada): " << duracion * 1000 << " ms (" << duracion << " s)" << endl;
+            for(int i=0; i<busquedas; i++){
+            	clock_t inicio = clock();
+            	busquedaSecuOrd(arr, tamanio, valor);
+            	clock_t fin = clock();
+            	tiempoBusquedaSecuOrd[i]= double(fin - inicio) / CLOCKS_PER_SEC * 1000;
+			}
+			for(int i=0; i<busquedas; i++){
+		    	cout << tiempoBusquedaSecuOrd[i] << endl;
+			}
             break;
-            
         case 2:
-            inicio = clock();
-            cout << "El valor " << valor << " esta en la posicion: " << busquedaBinaria(arr, tamanio, valor);
-            fin = clock();
-            duracion = double(fin - inicio) / CLOCKS_PER_SEC;
-            cout << endl << "Tiempo de ejecuciÃ³n (binaria): " << duracion * 1000 << " ms (" << duracion << " s)" << endl;
+            for(int i=0; i<busquedas; i++){
+            	clock_t inicio = clock();
+            	busquedaBinaria(arr, tamanio, valor);
+            	clock_t fin = clock();
+            	tiempobusquedaBinaria[i]= double(fin - inicio) / CLOCKS_PER_SEC * 1000;
+			}
+			for(int i=0; i<busquedas; i++){
+		    	cout << tiempobusquedaBinaria[i] << " ms." << endl;
+			}
             break;
-            
         default:
             cout << "No valido.";
             break;
     }
-    cout << endl;
+    
     return 0;
 }
 
@@ -63,28 +91,27 @@ void ordenarBidireccional(int a[], int n){
 	
 	int izq=0;
 	int der=n-1;
-	int k=n;
+	int k=n-1;
 	
-	while(izq<=der){
-		
-		for(int i=der; i>izq; i--){
+	while(izq<=der){//para que ambas partes de la mitad se hallan recorrido
+		for(int i=der; i>izq; i--){ //  |izq=0			n=der|
 			if(a[i-1]>a[i]){
 				int aux=a[i-1];
 				a[i-1]=a[i];
 				a[i]=aux;
-				k=i;
+				k=i; //cantidad de elementos ordenados en la izq
 			}
 		}
 		izq=k;
 		
-		for(int i=izq; i<der; i++){
-			if(a[i-1]>a[i]){
-				int aux=a[i-1];
-				a[i-1]=a[i];
-				a[i]=aux;
-				k=i;
-			}
-		}
+		for(int i=izq; i<der; i++){ //  |izq=0			n=der|
+			if (a[i] > a[i + 1]) {
+                int aux = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = aux;
+                k = i + 1;  // Actualiza k a la última posición de intercambio
+            }
+        }
 		der=k-1;
 	}
 }
